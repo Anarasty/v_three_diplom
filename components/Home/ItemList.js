4ada668cdce51d8fa910111074ee8f9a19df91b9
Anarasty <murloc.ninja16@gmail.com>
2025-04-12 23:12:58 +0300

    display category check

--- components/Home/ItemList.js
+++ components/Home/ItemList.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Data from '../../shared/Data'
 
 function ItemList() {
+    const [items, setItems]=useState([]);
+    useEffect(()=>{
+        setItems(Data.ItemList)
+    },[])
   return (
-    <div>
-      ItemList
+    <div className='row row-cols-3 row-cols-sm-4 row-cols-md-5 row-cols-lg-7 mt-4'>
+      {items.map((item)=>(
+        <div style={{cursor: 'pointer'}} className='d-flex flex-column align-items-center cursor_pointer_custom'>
+            <img src={item.image} width={45} height={45}></img>
+            <h6 className='
+            text-center'>{item.name}</h6>
+        </div>
+      ))}
     </div>
   )
 }
---
+++ shared/Data.js
@@ -0,0 +1,43 @@
+const ItemList=[
+    {
+        id:1,
+        name:'Documents',
+        image:'./Images/documentation.png'
+    },
+    {
+        id:2,
+        name:'Keys',
+        image:'./Images/key-chain.png'
+    },
+    {
+        id:3,
+        name:'Tech',
+        image:'./Images/smartphone.png'
+    },
+    {
+        id:4,
+        name:'Wallets',
+        image:'./Images/wallet.png'
+    },
+    {
+        id:5,
+        name:'Clothes',
+        image:'./Images/tshirt.png'
+    },
+    {
+        id:6,
+        name:'Bags',
+        image:'./Images/school-bag.png'
+    },
+    {
+        id:7,
+        name:'Other',
+        image:'./Images/other.png'
+    },
+
+
+]
+
+export default{
+    ItemList
+} 
