86ab35f86abd2dbfb068b3306d0009507065f8d5
Anarasty <murloc.ninja16@gmail.com>
2025-04-14 20:55:56 +0300

    sussesful post management

--- components/Header.js
+++ components/Header.js
@@ -1,49 +1,78 @@
-import Image from 'next/image'
-import React from 'react'
-import { useSession, signIn, signOut } from "next-auth/react"
-import Link from 'next/link';
+import Image from "next/image";
+import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
 
 import { BsCheck2Square } from "react-icons/bs";
 import { BsArrowRightSquareFill } from "react-icons/bs";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const USER_IMAGE='https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg'
+const USER_IMAGE =
+  "https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg";
 function Header() {
   const router = useRouter();
-  const { data: session } = useSession()
-  console.log("Session",session)
-  
+  const { data: session } = useSession();
+  console.log("Session", session);
+
   const handleClick = () => {
     if (session) {
-      router.push('/create-post');
+      router.push("/create-post");
     } else {
-      toast.warning('LOG IN TO CREATE YOUR POST!');
+      toast.warning("LOG IN TO CREATE YOUR POST!");
     }
   };
 
   return (
-    <div className='d-flex justify-content-between p-3 border-bottom border-primary'>
+    <div className="d-flex justify-content-between p-3 border-bottom border-primary">
       <Link href="/">
-  <img src="/Images/logo2.png" width={150} alt="test2" style={{ cursor: 'pointer' }} />
-</Link>
-      <div className='d-flex gap-4 align-items-center'>
-        <button onClick={handleClick} className='btn btn-primary'><span className="d-none d-md-inline">Post Item</span> <BsCheck2Square className="d-inline d-md-none"/>
+        <img
+          src="/Images/logo2.png"
+          width={150}
+          alt="test2"
+          style={{ cursor: "pointer" }}
+        />
+      </Link>
+      <div className="d-flex gap-4 align-items-center">
+        <button onClick={handleClick} className="btn btn-primary">
+          <span className="d-none d-md-inline">Post Item</span>{" "}
+          <BsCheck2Square className="d-inline d-md-none" />
         </button>
 
         <ToastContainer position="top-center" autoClose={3000} />
 
-       {!session?<button className='btn btn-outline-primary' onClick={()=>signIn()}><span className="d-none d-md-inline">Sign In</span> <BsArrowRightSquareFill className="d-inline d-md-none"/>
-        </button>
-        :<button className='btn btn-outline-primary' onClick={()=>signOut()}><span className="d-none d-md-inline">Sign Out</span> <BsArrowRightSquareFill className="d-inline d-md-none"/>
-        </button>}
+        {!session ? (
+          <button className="btn btn-outline-primary" onClick={() => signIn()}>
+            <span className="d-none d-md-inline">Sign In</span>{" "}
+            <BsArrowRightSquareFill className="d-inline d-md-none" />
+          </button>
+        ) : (
+          <button className="btn btn-outline-primary" onClick={() => signOut()}>
+            <span className="d-none d-md-inline">Sign Out</span>{" "}
+            <BsArrowRightSquareFill className="d-inline d-md-none" />
+          </button>
+        )}
 
-        <Image src={session?session?.user?.image:USER_IMAGE} width={40} height={40} alt={'test1'} className='rounded-circle'/>
+        <Image
+          style={{ cursor: "pointer" }}
+          src={session ? session?.user?.image : USER_IMAGE}
+          width={40}
+          height={40}
+          alt={"test1"}
+          className="rounded-circle"
+          onClick={() => {
+            if (session) {
+              router.push("/profile");
+            } else {
+              toast.warning("LOG IN TO VIEW YOUR PROFILE!");
+            }
+          }}
+        />
       </div>
-    </div> 
-  )
+    </div>
+  );
 }
 
-export default Header
+export default Header;
---
+++ pages/profile/index.js
@@ -0,0 +1,101 @@
+import { useSession } from "next-auth/react";
+import React, { useEffect, useState } from "react";
+import app from "../../shared/FirebaseConfig";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
+import PostItem from '../../components/Home/PostItem';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function Profile() {
+  const { data: session } = useSession();
+  const [userPost, setUserPost]=useState([])
+  const db = getFirestore(app);
+
+  useEffect(() => {
+    getUserPost();
+  }, [session]);
+
+  const getUserPost = async () => {
+    if(session?.user.email){
+      const q = query(
+        collection(db, "posts"),
+        where("email", "==", session?.user.email)
+      );
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((postDoc) => {
+        //console.log(doc.id, " => ", doc.data());
+        let data=postDoc.data()
+        data.id=postDoc.id
+        setUserPost(userPost=>[...userPost,data])
+      });
+    }
+  };
+
+  const onDeletePost=async(id)=>{
+    try {
+      await deleteDoc(doc(db, "posts", id));
+      toast.success("Post successfully deleted!");
+      setUserPost(userPost.filter(post => post.id !== id)); // Обновляем список без перезагрузки
+    } catch (error) {
+      console.error("Error deleting post:", error);
+      toast.error("Failed to delete post. Please try again.");
+    }
+  }
+
+  return (
+    <div className="container mt-4">
+  <h2 className="h3 fw-bold text-primary mb-3">Profile</h2>
+  <p className="mb-4">Manage your posts</p>
+
+  <div className="row g-4">
+    {userPost.map((item) => (
+      <div key={item.id} className="col-lg-4 col-md-6 col-sm-12">
+      <div className="card h-100 shadow-sm border-0 d-flex flex-column" style={{ minHeight: '500px',width: "20rem", }}>
+        <PostItem post={item} />
+    
+        <div className="mt-auto p-3">
+          <button
+            className="btn btn-danger w-100"
+            onClick={() => onDeletePost(item.id)}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+    ))}
+  </div>
+
+  <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
+
+</div>
+  );
+}
+
+export default Profile;
+
+
+// OLD DIV
+{/* <div className="p-4 mt-4 col-lg-4 col-md-6">
+      <h2 className="h3 fw-bold text-primary">Profile</h2>
+      <p>Manage your posts</p>
+      <div>
+        {userPost&&userPost?.map((item)=>(
+          <div>
+            <p>{item.title}</p>
+            <PostItem post={item}/>
+            <button className='bg-red-400 w-full p-1 mt-1
+        rounded-md text-white' 
+       onClick={()=>onDeletePost(item.id)}>Delete</button>
+          </div>
+        ))}
+      </div>
+    </div> */}
